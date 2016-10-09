6873c0ee7f078f36556c46cece45183fc63c611b
tritone11 <gioele@officinetesla.it>
2016-10-09 20:53:53 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function myInit() {
+    /*new Vivus("my-svg", {
+        type: "delayed",
+        pathTimingFunction: Vivus.EASE_OUT,
+        animTimingFunction: Vivus.EASE_IN,
+        duration: 200,
+        start: "autostart",
+        delay: 2
+    },function(){document.getElementById('myG').classList.add("finished")})*/
+}
